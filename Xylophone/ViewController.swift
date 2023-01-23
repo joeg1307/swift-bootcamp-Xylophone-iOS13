a3d82abbe5905dbf2ee000c97af5a1b34bef383a
Horst Josef Grenz Meza <jgrenz@outlook.com>
2023-01-23 09:11:18 -0800

    implemented missing code that enables xylophone functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let title = sender.currentTitle!
+        let msg = title+" got pressed"
+        print(msg)
+        sender.alpha = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+        playSound(key:title)
 
+    }
+    func playSound(key: String){
+        let url = Bundle.main.url(forResource: key.uppercased(), withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
